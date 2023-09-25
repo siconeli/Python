3c8b99c435bbfb86a02f33a865851c1e5b10d2ff
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2023-09-25 17:54:47 -0400

    estudando

---
+++ funcoes.py
@@ -0,0 +1,25 @@
+# def diz_oi():
+#     print('oi')
+
+# diz_oi()
+
+# def cantar_parabens():
+#     print('Parabéns pra você')
+#     print('Nesta data querida')
+
+# cantar_parabens()
+
+# refatorar = 'Escrever de uma forma melhor'
+
+# def parabens():
+#     return 'Parabéns!'
+
+# print(parabens())
+
+def varios():
+    num1 = 1
+    num2 = 2
+    
+    return num1, num2
+
+print(varios())
