14e3bc9300bb4712125c318f3b68b71d196f9510
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2023-09-26 16:37:15 -0400

    update

--- funcoes.py
+++ funcoes.py
@@ -16,10 +16,58 @@
 
 # print(parabens())
 
-def varios():
-    num1 = 1
-    num2 = 2
+# def varios():
+#     num1 = 1
+#     num2 = 2
     
-    return num1, num2
+#     return num1, num2
 
-print(varios())
+# print(varios())
+
+# def soma(numero):
+#     return numero + numero
+
+# print(soma())
+
+# total = 0
+# print(total)
+
+# def teste():
+#     global total
+#     total += 1
+#     return total
+
+# print(teste())
+# print(teste())
+# print(total)
+
+# def teste():
+#     """
+#         Função para testar a documentação
+
+#     """
+#     return 'fuck'
+
+
+# def soma(*args): transforma em tupla
+#     return sum(args)
+
+# numeros = [1, 2, 3, 4, 5]
+
+# print(soma(*numeros))
+
+# def cores(**kwargs): # Transforma em dicionario
+#     print(kwargs)
+
+# cores(henrique='azul', stefhany='rosa', benicio='verde')
+
+# def mostra_nomes(a, b, c, *args, exemplo=2, **kwargs):
+#     return a, b, c, args, exemplo, kwargs
+# a = 1
+# b = 2
+# c = 3
+# argss = 13
+# exemplo = 'teste'
+# nomes = {'nome': 'Henrique', 'sobrenome': 'Siconeli'}
+
+# print(mostra_nomes(a, b, c, argss, **nomes))
